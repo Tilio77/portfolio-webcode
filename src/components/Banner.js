235265540a224a11926d4a114fc5a8d9b58c7112
Tilio77 <tiliobolso1@gmail.com>
2022-11-17 01:28:31 -0300

    Actualizado

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -46,10 +46,10 @@ export const Banner = () => {
 			<Container>
 				<Row className="align-items-center">
 					<Col xs={12} md={6} xl={7}>
-						<span className="tagline">Welcome to my Portfolio</span>
-						<h1>{`Hi I'm webcoded `}<span className="wrap">{text}</span></h1>
+						<span className="tagline">Bienvenido a mi portafolio</span>
+						<h1>{`Hola soy codificador `}<span className="wrap">{text}</span></h1>
 						<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-						<button onClick={() => console.log("connect")}>Let's connect <ArrowRightCircle  size={25} /></button>
+						<button onClick={() => console.log("connect")}>Conectemos <ArrowRightCircle  size={25} /></button>
 					</Col>
 					<Col xs={12} md={6} xl={5}>
 						<img src={headerImg} alt="Header Img" />
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -39,17 +39,17 @@ export const NavBar = () => {
         </Navbar.Toggle>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
-            <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
-            <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
+            <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Inicio</Nav.Link>
+            <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Habilidades</Nav.Link>
+            <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Proyectos</Nav.Link>
           </Nav>
           <span className="navbar-text">
           	<div className="social-icon">
-          		<a href="#"><img src={navIcon1} alt="" /></a>
-          		<a href="#"><img src={navIcon2} alt="" /></a>
-          		<a href="#"><img src={navIcon3} alt="" /></a>
+          		<a href="https://www.linkedin.com/in/valentin-quijano-bb8893241/" target="_blank" rel="noreferrer noopener"><img src={navIcon1} alt="LinkedIn" /></a>
+          		<a href="https://www.facebook.com/TilioBolso" target="_blank" rel="noreferrer noopener"><img src={navIcon2} alt="Facebook" /></a>
+          		<a href="https://www.instagram.com/tilio_bolso/" target="_blank" rel="noreferrer noopener"><img src={navIcon3} alt="Instagram" /></a>
           	</div>
-          	<button className="vvd" onClick={() => console.log("connect")}><span>Let's Connect</span></button>
+          	<button className="vvd" onClick={() => console.log("connect")}><span>Conectemos</span></button>
           </span>
         </Navbar.Collapse>
       </Container>
